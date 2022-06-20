98885136c096551e76d301c835234cbf9536c504
Linhnguyen02 <ngglinh0914@gmail.com>
2022-06-20 11:48:21 +0700

    fix

--- js/slick.js
+++ js/slick.js
@@ -15,3 +15,10 @@ $('.banner').slick({
     autoplay: true,
     autoplaySpeed: 2000,
   });
+
+  $('.auto-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  })
